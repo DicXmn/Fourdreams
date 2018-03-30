ef4cb5b24338a065642af94968ed49427036084b
DicXmn <Daniel_112@live.com.mx>
2018-03-29 22:34:53 -0600

    Add files via upload

--- assets/js/fourdreams.js
+++ assets/js/fourdreams.js
@@ -1,1 +1,15 @@
+// Autor: davidtmiller
 
+var navbarCollapse = function() {
+    if ($("nav").offset().top > 150) {
+      $("nav").addClass("navbar-shrink");
+    } else {
+      $("nav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  
